9f2794c0bdbbecd75ea45e8cec2b55e9ca10f6ed
Anoshan Jayahanthan <101160077+AnoshanJ@users.noreply.github.com>
2026-06-03 10:20:41 +0530

    Refactor Customer Support Agent for PostgreSQL Migration and API Enhancements
    Signed-off-by: Anoshan Jayahanthan <101160077+AnoshanJ@users.noreply.github.com>

--- customer_support_agent/agent/agent.py
+++ customer_support_agent/agent/agent.py
@@ -1,5 +1,5 @@
 # %%
-# from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable
 from langchain_openai import ChatOpenAI
@@ -28,12 +28,11 @@ class Assistant:
             # If the LLM happens to return an empty response, we will re-prompt it
             # for an actual response.
             if not result.tool_calls and (
-                    not result.content
-                    or isinstance(result.content, list)
-                    and not result.content[0].get("text")
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
             ):
-                messages = state["messages"] + \
-                    [("user", "Respond with a real output.")]
+                messages = state["messages"] + [("user", "Respond with a real output.")]
                 state = {**state, "messages": messages}
             else:
                 break
@@ -59,7 +58,7 @@ def create_agent():
     ).partial(time=datetime.now)
 
     _tools = [
-        # TavilySearchResults(max_results=1),
+        TavilySearchResults(max_results=1),
         fetch_user_flight_information,
         search_flights,
         lookup_policy,
